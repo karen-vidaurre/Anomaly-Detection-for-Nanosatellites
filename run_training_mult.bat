eeae52635751b7763717c8f986d0327f60ca2c09
karenvida <karen.vidaurre@ucb.edu.bo>
2026-03-02 16:03:37 +0900

    feat: Multilabel with mask on test

--- run_training_mult.bat
+++ run_training_mult.bat
@@ -14,11 +14,20 @@ for %%w in (%WINDOWS%) do (
     @REM echo Done window size %%w
     @REM echo.
 
+    @REM echo ======================================
+    @REM echo Wavelet multilabel LR classification for window size %%w
+    @REM echo ======================================
+
+    @REM python scripts\ML2_wav_LR.py --window_size %%w
+
+    @REM echo Done window size %%w
+    @REM echo.
+
     echo ======================================
-    echo Wavelet multilabel LR classification for window size %%w
+    echo Wavelet multilabel LR classification masked for window size %%w
     echo ======================================
 
-    python scripts\ML2_wav_LR.py --window_size %%w
+    python scripts\ML3_wav_LR_test_mask.py --window_size %%w
 
     echo Done window size %%w
     echo.
